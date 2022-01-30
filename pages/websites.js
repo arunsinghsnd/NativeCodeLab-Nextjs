4134eef7c4d68dad31f3f31000a829f3aa497fc7
arunsinghsnd <arunsinghsnd@gmail.com>
2022-01-30 18:06:06 +0530

    Websites page fixed

--- pages/websites.js
+++ pages/websites.js
@@ -14,8 +14,14 @@ import {
 } from "@material-ui/core";
 
 import CallToAction from "../src/ui/CallToAction";
+const backArrow = "static/assets/backArrow.svg";
+const forwardArrow = "static/assets/forwardArrow.svg";
+const analytics = "static/assets/analytics.svg";
+const seo = "static/assets/seo.svg";
+const outreach = "static/assets/outreach.svg";
+const ecommerce = "static/assets/ecommerce.svg";
 
-import Link from "../src/Link";
+import Link from "../src/ui/Link";
 
 const useStyles = makeStyles(theme => ({
   heading: {
@@ -56,19 +62,14 @@ const Websites = props => {
           content="Completely custom designed and built from scratch to be blazing fast. Optimized code, server-side rendering, and perfect responsive design | 99% PageSpeed Score"
         />
         <meta
-          property="og:title"
-          content="Bringing West Coast Technology to the Midwest | Websites"
-          key="og:title"
-        />
-        <meta
           property="og:url"
           key="og:url"
           content="nativecodelab.netlify.app/websites"
         />
         <link
+          href="nativecodelab.netlify.app/websites"
           rel="canonical"
           key="canonical"
-          href="nativecodelab.netlify.app/websites"
         />
       </Head>
       <Grid
@@ -89,37 +90,42 @@ const Websites = props => {
               style={{ backgroundColor: "transparent" }}
               component={Link}
               href="/mobileapps"
-              onClick={() => {
-                props.setSelectedIndex(2);
-              }}
+              onClick={() => props.setSelectedIndex(2)}
             >
               <img
-                src="/assets/backArrow.svg"
-                alt="Back to Aandroid and IOS Developemnt page "
+                src={backArrow}
+                alt="Back to iOS/Android App Development Page"
               />
             </IconButton>
           </Grid>
         </Hidden>
-
         <Grid item container direction="column" className={classes.heading}>
           <Grid item>
-            <Typography align={matchesMD ? "center" : undefined} variant="h1">
-              Website Developemnt
+            <Typography
+              gutterBottom={matchesXS}
+              style={{
+                lineHeight: matchesXS ? 1.2 : undefined,
+                fontSize: matchesXS ? "2.25em" : undefined,
+              }}
+              align={matchesMD ? "center" : undefined}
+              variant="h1"
+            >
+              Website Development
             </Typography>
           </Grid>
           <Grid item>
             <Typography
-              variant="body2"
-              align={matchesSM ? "center" : undefined}
+              align={matchesMD ? "center" : undefined}
+              variant="body1"
               paragraph
             >
-              Having a website is a necessity in today's business world. They
-              give you one central, location to let pepole know who you are,
-              what you do, and why yo're the best at it.
+              Having a website is a necessity in today’s business world. They
+              give you one central, public location to let people know who you
+              are, what you do, and why you’re the best at it.
             </Typography>
             <Typography
               align={matchesMD ? "center" : undefined}
-              variant="body2"
+              variant="body1"
               paragraph
             >
               From simply having your hours posted to having a full fledged
@@ -134,14 +140,9 @@ const Websites = props => {
               style={{ backgroundColor: "transparent" }}
               component={Link}
               href="/services"
-              onClick={() => {
-                props.setSelectedIndex(0);
-              }}
+              onClick={() => props.setSelectedIndex(0)}
             >
-              <img
-                src="/assets/forwardArrow.svg"
-                alt="Forwared to Services page"
-              />
+              <img src={forwardArrow} alt="Forward to Services Page" />
             </IconButton>
           </Grid>
         </Hidden>
@@ -152,14 +153,14 @@ const Websites = props => {
         direction={matchesSM ? "column" : "row"}
         alignItems="center"
         className={classes.rowContainer}
-        style={{ marginTop: "10em" }}
+        style={{ marginTop: "15em" }}
       >
         <Grid item>
           <Grid container direction="column">
             <Grid item>
               <Typography
+                align={matchesSM ? "center" : undefined}
                 variant="h4"
-                align={matchesMD ? "center" : undefined}
                 gutterBottom
               >
                 Analytics
@@ -167,22 +168,18 @@ const Websites = props => {
             </Grid>
             <Grid item>
               <img
-                src="/assets/analytics.svg"
+                src={analytics}
                 style={{ marginLeft: "-2.75em" }}
-                alt="grap with mangnifying glass revealing 1's and 0's"
+                alt="graph with magnifying glass revealing 1's and 0's"
               />
             </Grid>
           </Grid>
         </Grid>
         <Grid item className={classes.paragraphContainer}>
-          <Typography
-            variant="body2"
-            align={matchesSM ? "center" : undefined}
-            gutterBottom
-          >
+          <Typography align={matchesSM ? "center" : undefined} variant="body1">
             Knowledge is power, and data is 21st Century gold. Analyzing this
-            data can reveal hidden patterns and treads in your business,
-            empowering you to make smarter descision with measurable effects.
+            data can reveal hidden patterns and trends in your business,
+            empowering you to make smarter decisions with measurable effects.
           </Typography>
         </Grid>
       </Grid>
@@ -193,24 +190,17 @@ const Websites = props => {
         alignItems="center"
         justifyContent="flex-end"
         className={classes.rowContainer}
-        style={{ marginTop: "10em", marginBottom: "10em" }}
+        style={{ marginBottom: "15em", marginTop: "15em" }}
       >
         <Grid item>
           <Grid container direction="column">
             <Grid item>
-              <Typography
-                align={matchesMD ? "center" : undefined}
-                variant="h4"
-                gutterBottom
-              >
-                E-commerce
+              <Typography align="center" variant="h4" gutterBottom>
+                E-Commerce
               </Typography>
             </Grid>
             <Grid item>
-              <img
-                src="/assets/ecommerce.svg"
-                alt="grap with mangnifying glass revealing 1's and 0's"
-              />
+              <img src={ecommerce} alt="world outline made of dollar signs" />
             </Grid>
           </Grid>
         </Grid>
@@ -220,18 +210,18 @@ const Websites = props => {
           className={classes.paragraphContainer}
         >
           <Typography
-            variant="body2"
             align={matchesSM ? "center" : undefined}
+            variant="body1"
             paragraph
           >
-            It's no secret that pepole like to shop online.
+            It’s no secret that people like to shop online.
           </Typography>
           <Typography
-            variant="body2"
             align={matchesSM ? "center" : undefined}
+            variant="body1"
             paragraph
           >
-            In 2017 over $2.3 trillion was spent in e-commerce, and it's time
+            In 2017 over $2.3 trillion was spent in e-commerce, and it’s time
             for your slice of that pie.
           </Typography>
         </Grid>
@@ -247,7 +237,7 @@ const Websites = props => {
           <Grid container direction="column">
             <Grid item>
               <Typography
-                align={matchesMD ? "center" : undefined}
+                align={matchesSM ? "center" : undefined}
                 variant="h4"
                 gutterBottom
               >
@@ -255,23 +245,19 @@ const Websites = props => {
               </Typography>
             </Grid>
             <Grid item>
-              <img src="/assets/outreach.svg" alt="megaphone" />
+              <img src={outreach} alt="megaphone" />
             </Grid>
           </Grid>
         </Grid>
         <Grid
           item
-          style={{ marginLeft: matchesSM ? 0 : "1em" }}
           className={classes.paragraphContainer}
+          style={{ marginLeft: matchesSM ? 0 : "1em" }}
         >
-          <Typography
-            variant="body2"
-            align={matchesSM ? "center" : undefined}
-            gutterBottom
-          >
-            Draw people in with a dazzling websites. Showing off your products
-            online is a great way to help customsers decide what's right for
-            them before visiting in person.
+          <Typography align={matchesSM ? "center" : undefined} variant="body1">
+            Draw people in with a dazzling website. Showing off your products
+            online is a great way to help customers decide what’s right for them
+            before visiting in person.
           </Typography>
         </Grid>
       </Grid>
@@ -282,25 +268,19 @@ const Websites = props => {
         alignItems="center"
         justifyContent="flex-end"
         className={classes.rowContainer}
-        style={{ marginTop: "10em", marginBottom: "10em" }}
+        style={{ marginTop: "15em", marginBottom: "15em" }}
       >
         <Grid item>
           <Grid container direction="column">
             <Grid item>
-              <Typography
-                align={matchesMD ? "center" : undefined}
-                variant="h4"
-                gutterBottom
-              >
-                Search Engine <br />
+              <Typography align="center" variant="h4" gutterBottom>
+                Search Engine
+                <br />
                 Optimization
               </Typography>
             </Grid>
             <Grid item>
-              <img
-                src="/assets/seo.svg"
-                alt="wesites standing on winner's podium"
-              />
+              <img src={seo} alt="website standing on winner's podium" />
             </Grid>
           </Grid>
         </Grid>
@@ -310,26 +290,26 @@ const Websites = props => {
           className={classes.paragraphContainer}
         >
           <Typography
-            variant="body2"
             align={matchesSM ? "center" : undefined}
+            variant="body1"
             paragraph
           >
-            How often you ever been to the second page of google results?
+            How often have you ever been to the second page of Google results?
           </Typography>
           <Typography
-            variant="body2"
             align={matchesSM ? "center" : undefined}
+            variant="body1"
             paragraph
           >
-            If you're like us, probably never.
+            If you’re like us, probably never.
           </Typography>
           <Typography
-            variant="body2"
             align={matchesSM ? "center" : undefined}
+            variant="body1"
             paragraph
           >
-            Customer don't go there either, so we make sure website is desgined
-            to end up on top.
+            Customers don’t go there either, so we make sure your website is
+            designed to end up on top.
           </Typography>
         </Grid>
       </Grid>
